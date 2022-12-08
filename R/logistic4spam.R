b620a68f15bd90fb83d6852fd7d548afa399b0e6
myccmj <myccmj@163.com>
2022-12-08 18:01:45 +0800

    R语言拟合逻辑斯蒂
    用R拟合逻辑斯蒂在spam上

---
+++ R/logistic4spam.R
@@ -0,0 +1,25 @@
+library(ISLR)
+setwd("E:/北大新资料/统计学习")
+Spam.train <- read.table(file.path(getwd(), "spam.train"))
+n_1=0
+for(i in 1:4601)
+{
+  if(Spam.train$V58[i]==1)
+    n_1=n_1+1
+}
+summary(Spam.train)
+glm.fit=glm(V58~.,data=Spam.train,family=binomial)
+glm.probs=predict(glm.fit,newdata=Spam.train)
+glm.pred=rep(0,4601)
+glm.pred[glm.probs>.5]=1
+err=glm.pred-Spam.train$V58
+n01=0
+n10=0
+for(i in 1:4601)
+{
+  if(err[i]==-1)
+    n01=n01+1
+  if(err[i]==1)
+    n10=n10+1
+}
+table(glm.pred,Direction)
