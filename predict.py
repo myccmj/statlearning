1c34f01c7aae49fde89d9ade07ad81300dc67436
kezhifa1006 <106938126+kezhifa1006@users.noreply.github.com>
2022-12-23 20:49:43 +0800

    new

--- predict.py
+++ predict.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  8 21:10:00 2022
+@author: myccm
+"""
+
 import scipy.io as sio
 import numpy as np
 import torch
@@ -18,7 +24,6 @@ def config():
     parser.add_argument("--bs_ratio", default=0.05, type=float)
     parser.add_argument("--seed", default=0, type=int)             
     parser.add_argument("--epochs", default=1000, type=int)
-    parser.add_argument("--file_name", default="")
     args = parser.parse_args()
 
     return args
@@ -30,8 +35,8 @@ def load_data(args):
         data = sio.loadmat('./data/spambase.mat')
         x_data = data['X'].astype(np.float32)
         y_data = data['y'].astype(np.float32)
-        ind_0 = np.reshape(data['y']==-1, -1)
-        ind_1 = np.reshape(data['y']==1, -1)
+        ind_0 = np.reshape(data['y']==1, -1)#垃圾邮件
+        ind_1 = np.reshape(data['y']==-1, -1)#正常邮件
     
     return x_data[ind_0, :], x_data[ind_1, :], y_data[ind_0, :], y_data[ind_1, :]
 
@@ -44,14 +49,14 @@ if __name__ == "__main__":
     torch.manual_seed(args.seed)
 
     x_data_0, x_data_1, y_data_0, y_data_1 = load_data(args)
-    model = Model(x_data_0.shape[1], args.delta, args.lr, args.classifier, args.method)
-    if args.train_type == 'base':
-        train_op = train.Base_train(model, x_data_0, x_data_1, y_data_0, y_data_1, args.epochs, args.split_rate, args.bs_ratio)
+    model = Model(x_data_0.shape[1], 0.12, args.lr, 'linear_logistic', args.method)
     
-    train_op.train()
-    train_op.feature_selection()
-    file_name = args.classifier + args.file_name
-    train_op.save_results(file_name=file_name)
-
-    
-
+    model.load('logistic')
+    predict_y1=model.predict(torch.tensor(x_data_1[:10]),1)
+    print(predict_y1)
+    # predict_y0=model.predict(torch.tensor(x_data_0))
+    # predict_y1=model.predict(torch.tensor(x_data_1))
+    # errn_01=sum([predict_y0[i]!=y_data_0[i][0] for i in range(len(y_data_0))])
+    # print("垃圾邮件：","分类错误",errn_01,"分类正确",len(y_data_0)-errn_01)
+    # errn_10=sum([predict_y1[i]!=y_data_1[i][0] for i in range(len(y_data_1))])
+    # print("正常邮件：","分类错误",errn_10,"分类正确",len(y_data_1)-errn_10)
